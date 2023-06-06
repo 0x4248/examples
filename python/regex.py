306d5b387dfd3ca2bf78b70e61016d6f0f795f8c
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-06 17:04:43 +0000

    Format python code with black

--- python/regex.py
+++ python/regex.py
@@ -9,21 +9,21 @@ import re
 text = "The quick brown fox jumps over the lazy dog 12345 !@#$"
 
 # Extract the first word
-first_word = re.search(r'\b\w+\b', text).group()
+first_word = re.search(r"\b\w+\b", text).group()
 print("First word:", first_word)
 
 # Extract the last word
-last_word = re.search(r'\b\w+\b$', text).group()
+last_word = re.search(r"\b\w+\b$", text).group()
 print("Last word:", last_word)
 
 # Extract the numbers
-numbers = re.findall(r'\d+', text)
+numbers = re.findall(r"\d+", text)
 print("Numbers:", numbers)
 
 # Extract the letters
-letters = re.findall(r'[a-zA-Z]+', text)
+letters = re.findall(r"[a-zA-Z]+", text)
 print("Letters:", letters)
 
 # Extract the symbols
-symbols = re.findall(r'[^\w\s]+', text)
+symbols = re.findall(r"[^\w\s]+", text)
 print("Symbols:", symbols)
