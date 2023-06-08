5396e8e5c5b7f03f47e0559231311363d1b7efa2
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-08 19:01:32 +0100

    Add more examples
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

---
+++ javascript/date.js
@@ -0,0 +1,8 @@
+/* date.js
+ * Prints the date and time in javascript
+ * Github: https://www.github.com/awesomelewis2007/examples
+ * License: GNU General Public License v3.0
+ * By: Lewis Evans
+ */
+
+console.log(Date());
---
+++ javascript/functions.js
@@ -0,0 +1,12 @@
+/* functions.js
+ * This program shows how to use functions in javascript
+ * Github: https://www.github.com/awesomelewis2007/examples
+ * License: GNU General Public License v3.0
+ * By: Lewis Evans
+ */
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 1));
