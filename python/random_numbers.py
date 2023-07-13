ac1def661b5cea703e1ee9900592a00926664d4b
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-13 17:15:59 +0100

    Add comments
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- python/random_numbers.py
+++ python/random_numbers.py
@@ -4,7 +4,9 @@
 # Licence: GNU General Public License v3.0
 # By: Lewis Evans
 
-import random
+import random # Import the random module
 
+# Generate five random numbers between 0 and 49
 for i in range(5):
+    # Print the random number
     print(random.randint(0, 49))
