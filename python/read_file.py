c077f5fb153ae92f05ccb9a6268ba7a943a36045
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-13 17:16:19 +0100

    Add comments
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- python/read_file.py
+++ python/read_file.py
@@ -4,6 +4,9 @@
 # Licence: GNU General Public License v3.0
 # By: Lewis Evans
 
+# Open the file "input.txt" in read mode and store the contents in a variable called contents
 with open("input.txt", "r") as file:
+    # Read the contents of the file
     contents = file.read()
+    # Print the contents of the file
     print(contents)
