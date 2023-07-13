41560726674ba74b51996aa2e885d28c7ee12404
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-13 17:16:56 +0100

    Add comments
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- python/rounding.py
+++ python/rounding.py
@@ -4,9 +4,15 @@
 # Licence: GNU General Public License v3.0
 # By: Lewis Evans
 
+# This program rounds numbers the user enters.
 while True:
+    # Ask the user to enter a number
     number = input("Enter a number: ")
+    # Convert the number to a float
     number = float(number)
+    # Get the number of decimal places
     decimal_places = len(str(number).split(".")[1])
+    # Round the number to the number of decimal places
     for i in range(decimal_places):
+        # Print the rounded number
         print("Round to", i, "decimal places:", round(number, i))
