b7692b59b7a632bc6b98b89e2fbce1f4ee9e4b6d
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-13 17:17:13 +0100

    Add comments
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- python/write_file.py
+++ python/write_file.py
@@ -4,5 +4,7 @@
 # Licence: GNU General Public License v3.0
 # By: Lewis Evans
 
+# Open the file "output.txt" in write mode and store it in a variable called file
 with open("output.txt", "w") as file:
+    # Write some text to the file
     file.write("This is some sample text that will be written to the file.")
