f8a4ddd3383a0227d0dad55af197ce0311609a9c
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-25 08:43:34 +0100

    Add time.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ python/time.py
@@ -0,0 +1,18 @@
+# time.py
+# This program shows how to the time module in python.
+# Github: https://www.github.com/lewisevans2007/examples
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+#import the time module
+import time 
+
+time.sleep(1) #This will pause the program for 1 second
+
+#This will print "Hello World!" to the screen after 1 second
+print("Hello World!")
+
+time.sleep(10) #This will pause the program for 10 seconds
+
+#This will print "Goodbye World!" to the screen after 10 seconds after the first print statement
+print("Goodbye World!")
