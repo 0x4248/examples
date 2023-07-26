fd49aac8c8179eb7de94770fa689a84d721cf47e
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-26 14:02:16 +0100

    Add casting.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ python/casting.py
@@ -0,0 +1,27 @@
+# casting.py
+# This program shows how casting works in python.
+# Github: https://www.github.com/lewisevans2007/examples
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+# In python you can cast variables to different types.
+
+# String to integer
+
+a = "1" # This is a string
+b = int(a) # This is an integer
+
+# String to float
+
+a = "1.0" # This is a string
+b = float(a) # This is a float
+
+# String to boolean
+
+a = "True" # This is a string
+b = bool(a) # This is a boolean
+
+# String to list
+
+a = "Hello World!" # This is a string
+b = list(a) # This is a list
