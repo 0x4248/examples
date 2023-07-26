20daf07153a69667814b07ff410d4a11843a53a6
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-26 13:03:51 +0000

    Format python code with black

--- python/casting.py
+++ python/casting.py
@@ -8,20 +8,20 @@
 
 # String to integer
 
-a = "1" # This is a string
-b = int(a) # This is an integer
+a = "1"  # This is a string
+b = int(a)  # This is an integer
 
 # String to float
 
-a = "1.0" # This is a string
-b = float(a) # This is a float
+a = "1.0"  # This is a string
+b = float(a)  # This is a float
 
 # String to boolean
 
-a = "True" # This is a string
-b = bool(a) # This is a boolean
+a = "True"  # This is a string
+b = bool(a)  # This is a boolean
 
 # String to list
 
-a = "Hello World!" # This is a string
-b = list(a) # This is a list
+a = "Hello World!"  # This is a string
+b = list(a)  # This is a list
