570e346ea13b01e355c47b0b8f2bf7c8c54a46b7
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-26 13:58:29 +0100

    Add types.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ python/types.py
@@ -0,0 +1,32 @@
+# types.py
+# This program shows the different types of variables in python.
+# Github: https://www.github.com/lewisevans2007/examples
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+# This is a string variable
+string = "Hello World!"
+
+# This is an integer variable
+integer = 1
+
+# This is a float variable
+float = 1.0
+
+# This is a boolean variable
+boolean = True
+
+# This is a list variable
+list = ["Hello", "World!"]
+
+# This is a tuple variable
+tuple = ("Hello", "World!")
+
+# This is a dictionary variable
+dictionary = {"Hello": "World!"}
+
+# This is a set variable
+set = {"Hello", "World!"}
+
+# This is a frozenset variable
+frozenset = frozenset({"Hello", "World!"})
